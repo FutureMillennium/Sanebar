6a94d9dc0b471a89173993d6e150a7459202e1d8
Zdeněk Gromnica <futuremillennium@gmail.com>
2015-04-07 01:11:17 +0200

    + Sanebar – detect active window change 	+ active window title 		+ detect title updates 		+ (no title) on no title

--- SaneBar/SanebarWindow.xaml.cs
+++ SaneBar/SanebarWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -21,6 +22,10 @@ namespace Sanebar
 	public partial class SanebarWindow : Window
 	{
 		SanebarWindow[] sanebarWindows;
+		WindowInteropHelper this32;
+		IntPtr hwndActiveWindow;
+		private string titleActiveWindow;
+		WinAPI.WinEventDelegate winEventDelegate;
 
 		public SanebarWindow() : this(true)
 		{
@@ -29,8 +34,10 @@ namespace Sanebar
 
 		public SanebarWindow(bool primary = true)
 		{
+			// Primary Sanebar window handles other Sanebar windows
 			if (primary)
 			{
+				// Create a Sanebar window for each monitor
 				sanebarWindows = new SanebarWindow[System.Windows.Forms.Screen.AllScreens.Length];
 				sanebarWindows[0] = this;
 				int i = 1;
@@ -39,13 +46,14 @@ namespace Sanebar
 				{
 					if (screen.Primary == true)
 					{
+						// Set size
 						this.Top = screen.WorkingArea.Top;
 						this.Left = screen.WorkingArea.Left;
-						//this.Width = screen.WorkingArea.Right - screen.WorkingArea.Left;
-						this.Width = 1920;
+						this.Width = screen.WorkingArea.Right - screen.WorkingArea.Left;
 					}
 					else
 					{
+						// Set size
 						sanebarWindows[i] = new SanebarWindow(false);
 						sanebarWindows[i].Top = screen.WorkingArea.Top;
 						sanebarWindows[i].Left = screen.WorkingArea.Left;
@@ -54,10 +62,84 @@ namespace Sanebar
 						i++;
 					}
 				}
+
+				winEventDelegate = new WinAPI.WinEventDelegate(WinEventProc);
+				IntPtr m_hhook = WinAPI.SetWinEventHook(WinAPI.EVENT_SYSTEM_FOREGROUND, WinAPI.EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, winEventDelegate, 0, 0, WinAPI.WINEVENT_OUTOFCONTEXT);
+				m_hhook = WinAPI.SetWinEventHook(WinAPI.EVENT_OBJECT_LOCATIONCHANGE, WinAPI.EVENT_OBJECT_NAMECHANGE, IntPtr.Zero, winEventDelegate, 0, 0, WinAPI.WINEVENT_OUTOFCONTEXT);
 			}
-			this.Height = SystemParameters.CaptionHeight;
+			this.Height = System.Windows.Forms.SystemInformation.CaptionHeight;
 
 			InitializeComponent();
+		}
+
+		private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+		{
+			// TEMP
+			if (e.ChangedButton == MouseButton.Right)
+				Application.Current.Shutdown();
+		}
+
+		private void Window_SourceInitialized(object sender, EventArgs e)
+		{
+			this32 = new WindowInteropHelper(this);
+
+			// can’t get focus (WS_EX_NOACTIVATE)
+			int exStyle = WinAPI.GetWindowLong(this32.Handle, WinAPI.GWL_EXSTYLE);
+			WinAPI.SetWindowLong(this32.Handle, WinAPI.GWL_EXSTYLE, exStyle | WinAPI.WS_EX_NOACTIVATE);
+		}
+
+		// Windows event
+		public void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+		{
+			switch (eventType)
+			{
+				// Active window changed
+				case WinAPI.EVENT_SYSTEM_FOREGROUND:
+					{
+						if (hwnd != this32.Handle)
+						{
+							hwndActiveWindow = hwnd;
+							ChangeTitle();
+							
+						}
+					}
+					break;
+				// Window name change
+				case WinAPI.EVENT_OBJECT_NAMECHANGE:
+					{
+						if (hwnd == hwndActiveWindow)
+						{
+							ChangeTitle();
+						}
+					}
+					break;
+				// Window location/size change
+				case WinAPI.EVENT_OBJECT_LOCATIONCHANGE:
+				{
+					
+				}
+					break;
+			}
+		}
+
+		// Change displayed active window title
+		private void ChangeTitle()
+		{
+			titleActiveWindow = WinAPI.GetWindowTitle(hwndActiveWindow);
+			if (string.IsNullOrWhiteSpace(titleActiveWindow))
+			{
+				titleActiveWindow = "(no title)";
+			}
+
+			foreach (SanebarWindow sanebarWindow in sanebarWindows)
+			{
+				sanebarWindow.ChangeTitle(titleActiveWindow);
+			}
+		}
+
+		internal void ChangeTitle(string title)
+		{
+			currentTitleLabel.Content = title;
 		}
 	}
 }
--- SaneBar/WinAPI.cs
+++ SaneBar/WinAPI.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,42 @@ namespace Sanebar
 {
 	class WinAPI
 	{
+		public const int WS_EX_NOACTIVATE = 0x08000000;
+		public const int GWL_EXSTYLE = -20;
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		public static extern int GetWindowLong(IntPtr hwnd, int index);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		public static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+
+
+		public const uint WINEVENT_OUTOFCONTEXT = 0;
+		public const uint EVENT_OBJECT_LOCATIONCHANGE = 0x800B; // hwnd ID idChild is moved/sized item
+		public const uint EVENT_OBJECT_NAMECHANGE = 0x800C; // hwnd ID idChild is item w/ name change
+		public const uint EVENT_SYSTEM_FOREGROUND = 3;
+
+		public delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+		[DllImport("user32.dll")]
+		public static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+
+
+		[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+		public static extern int GetWindowTextLength(IntPtr hWnd);
+
+		[DllImport("user32.dll")]
+		public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+		public static string GetWindowTitle(IntPtr hWnd)
+		{
+			// Allocate correct string length first
+			int length = GetWindowTextLength(hWnd);
+			StringBuilder sb = new StringBuilder(length + 1);
+			GetWindowText(hWnd, sb, sb.Capacity);
+			return sb.ToString();
+		}
 	}
 }
